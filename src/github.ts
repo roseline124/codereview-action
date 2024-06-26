2a42f13adf25253666eab61200d982a9c3efc632
roseline124 <guseod24@gmail.com>
2024-06-26 15:54:21 +0900

    chore: remove not used files

--- src/github.ts
+++
@@ -1,6 +0,0 @@
-// import { getOctokit } from "@actions/github";
-// import { Octokit } from "@octokit/rest";
-
-// const githubToken = process.env.GITHUB_TOKEN as string;
-
-// export const octokit = new Octokit({ auth: githubToken });
